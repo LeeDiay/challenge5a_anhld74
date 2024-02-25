9774989669fb76884339f1139d2ed349b7c736aa
LeeDiay <leducanh1503.works@gmail.com>
2024-02-25 21:55:32 +0700

    update syntax

--- makeDB.sql
+++ makeDB.sql
@@ -1,26 +1,26 @@
-CREATE DATABASE IF NOT EXISTS myproject;
+CREATE DATABASE myproject;
 
 USE myproject;
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    phone VARCHAR(20), NOT NULL
+    phone VARCHAR(20) NOT NULL,
     avatar VARCHAR(255),
     type VARCHAR(50)
 );
 
-CREATE TABLE IF NOT EXISTS post (
+CREATE TABLE post (
     id INT AUTO_INCREMENT PRIMARY KEY,
     uploader INT,
     content TEXT,
     post_time DATETIME
 );
 
-CREATE TABLE IF NOT EXISTS upload (
+CREATE TABLE upload (
     id INT AUTO_INCREMENT PRIMARY KEY,
     uploader VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
