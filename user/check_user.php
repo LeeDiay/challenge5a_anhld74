803369dc48e93054f30b27c27894975f1b43b702
LeeDiay <leducanh1503.works@gmail.com>
2024-02-26 14:46:38 +0700

    add page 403

--- user/check_user.php
+++ user/check_user.php
@@ -2,8 +2,6 @@
 if (empty($_SESSION['username'])) {
     // Điều hướng đến trang lỗi error_page.php
     header("Location: ../error_page.php");
-    //header("Location:../index.php");
-    // Đảm bảo không có mã HTML hoặc mã PHP nào được xuất ra sau dòng này
     exit;
 }
 ?>
